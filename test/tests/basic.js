a0c13a0a4164e3a267c1d16774101c25659ba835
John Hiesey <john@hiesey.com>
2015-07-02 00:50:43 -0700

    Clean up basic test

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -1,37 +1,22 @@
-var test = require('tape')
-// var crypto = require('crypto')
-var hashes = require('./hashes.json')
 var Buffer = require('buffer').Buffer
-// var sha1 = require('simple-sha1')
-
 var fs = require('fs');
+var test = require('tape')
+
 var reference = fs.readFileSync(__dirname + '/../server/static/basic.txt');
 
 var http = require('../..')
 
 test('basic functionality', function (t) {
-	// var hash = crypto.createHash('sha256')
-	// hash.setEncoding('hex')
-
 	http.get('/basic.txt', function (res) {
-		// var hash = crypto.createHash('sha256')
-		// hash.setEncoding('hex')
-
 		var buffers = []
 
 		res.on('end', function () {
-			// console.warn('end')
-			// hash.end()
 			t.ok(reference.equals(Buffer.concat(buffers)), 'contents match')
 			t.end()
 		})
 
 		res.on('data', function (data) {
-			// console.warn('data')
-			// console.log(data.toString('utf-8'))
 			buffers.push(data)
 		})
-
-		// res.pipe(hash)
 	})
 })

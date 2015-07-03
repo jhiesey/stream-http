8985094a2b98c99c050aa8f2966340a2b68f5beb
John Hiesey <john@hiesey.com>
2015-07-02 20:00:16 -0700

    Support binary requests in IE9

--- test/tests/binary.js
+++ test/tests/binary.js
@@ -1,6 +1,11 @@
 var Buffer = require('buffer').Buffer
 var fs = require('fs');
 var test = require('tape')
+var UAParser = require('ua-parser-js')
+
+var browserType = (new UAParser()).setUA(navigator.userAgent).getBrowser()
+// Binary data gets corrupted in IE8 or below
+var skipVerification = (browserType.name === 'IE' && browserType.major <= 8)
 
 var reference = fs.readFileSync(__dirname + '/../server/static/browserify.png');
 
@@ -11,7 +16,11 @@ test('binary download', function (t) {
 		var buffers = []
 
 		res.on('end', function () {
-			t.ok(reference.equals(Buffer.concat(buffers)), 'contents match')
+			if (skipVerification) {
+				t.skip('binary data not preserved on IE <= 8')
+			} else {
+				t.ok(reference.equals(Buffer.concat(buffers)), 'contents match')
+			}
 			t.end()
 		})
 

1a4a89a5e4d786a0238fa8f6f7373fe4cf834ad9
John Hiesey <john@hiesey.com>
2015-08-07 18:02:33 -0700

    Add test for data in `request.end()`

--- test/browser/post-text.js
+++ test/browser/post-text.js
@@ -26,3 +26,23 @@ test('post text', function (t) {
 	req.write(reference)
 	req.end()
 })
+
+test('post text with data in end()', function (t) {
+	var req = http.request({
+		path: '/echo',
+		method: 'POST'
+	}, function (res) {
+		var buffers = []
+
+		res.on('end', function () {
+			t.ok(reference.equals(Buffer.concat(buffers)), 'echoed contents match')
+			t.end()
+		})
+
+		res.on('data', function (data) {
+			buffers.push(data)
+		})
+	})
+
+	req.end(reference)
+})

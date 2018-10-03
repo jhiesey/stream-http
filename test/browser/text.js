e60ce5fb99d1c74e96120aa1086197c43fc100a8
John Hiesey <john@hiesey.com>
2018-10-02 21:45:10 -0700

    Replace erroneous test disable

--- test/browser/text.js
+++ test/browser/text.js
@@ -14,6 +14,7 @@ var skipResponseUrl = ((browserName === 'IE') ||
 	(browserName === 'Edge') ||
 	(browserName === 'Chrome' && browserVersion <= 36) ||
 	(browserName === 'Firefox' && browserVersion <= 31) ||
+	((browserName === 'Safari' || browserName === 'Mobile Safari') && browserVersion <= 8) ||
 	(browserName === 'Android Browser' && browserVersion <= 4))
 
 var reference = fs.readFileSync(__dirname + '/../server/static/basic.txt')

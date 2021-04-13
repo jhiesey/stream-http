2572062eae635f95322ba915b4d66dd5633b3679
John Hiesey <john@hiesey.com>
2021-04-13 16:27:08 -0700

    Add timeout test

--- test/browser/timeout.js
+++ test/browser/timeout.js
@@ -4,7 +4,7 @@ var test = require('tape')
 
 var http = require('../..')
 
-test('timeout', function (t) {
+test('requestTimeout', function (t) {
 	var req = http.get({
 		path: '/browserify.png?copies=5',
 		requestTimeout: 10 // ms
@@ -16,14 +16,14 @@ test('timeout', function (t) {
 		})
 	})
 	req.on('requestTimeout', function () {
-		t.pass('got timeout')
+		t.pass('got requestTimeout')
 		t.end()
 	})
 })
 
 // TODO: reenable this if there's a way to make it simultaneously
 // fast and reliable
-test.skip('no timeout after success', function (t) {
+test.skip('no requestTimeout after success', function (t) {
 	var req = http.get({
 		path: '/basic.txt',
 		requestTimeout: 50000 // ms
@@ -38,6 +38,26 @@ test.skip('no timeout after success', function (t) {
 		})
 	})
 	req.on('requestTimeout', function () {
-		t.fail('unexpected timeout')
+		t.fail('unexpected requestTimeout')
+	})
+})
+
+test('setTimeout', function (t) {
+	t.plan(2)
+
+	var req = http.get({
+		path: '/browserify.png?copies=5',
+	}, function (res) {
+		res.on('data', function (data) {
+		})
+		res.on('end', function () {
+			t.fail('request completed (should have timed out)')
+		})
+	})
+	req.setTimeout(10, function () {
+		t.pass('got setTimeout callback')
+	})
+	req.on('timeout', function () {
+		t.pass('got timeout')
 	})
 })
